ac3f1448a925ef021992b0804a25ee5d36da58a0
Mitchell Vivian <31528143+MellowYeller@users.noreply.github.com>
2020-11-27 13:55:54 -0800

    Complete jQuery demo functionality

---
+++ lab4/jQueryDemo.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+    $('#cat-button').on('click', event => {
+        $('.cat-picture').slideToggle();
+    })
+})
--- lab4/jQueryDemo.php
+++ lab4/jQueryDemo.php
@@ -3,6 +3,7 @@
     <head>
         <title>jQuery Demonstration</title>
         <link rel="stylesheet" type="text/css" href="/style.css">
+        <link rel="stylesheet" type="text/css" href="jQueryDemo.css">
     </head>
     <body>
         <div class="grid">
@@ -12,9 +13,13 @@
             <div class="main">
                 <fieldset>
                     <legend><h2>jQuery Demo</h2></legend>
-                    <p>Placeholder text :)</p>
+                    <p>Press the button below to view a jQuery animation.</p>
+                    <button id="cat-button">Toggle Cat</button>
+                    <img class="cat-picture" src="/res/avery.jpg" alt="My cat Avery!">
                 </fieldset>
             </div> <!--main-->
         </div> <!--grid-->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="jQueryDemo.js"></script>
     </body>
 </html>
